714dc3583bb552d43736ac5e331b472d657bd436
vamsikrishnakonidena379 <vamsikrishnakonidena79@gmail.com>
2026-01-30 07:22:11 -0500

    Add protected job routes with authentication middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express= require("express")
 const cors=require("cors")
 const authRoutes= require("./routes/auth.routes");
+const jobRoutes= require("./routes/jobs.routes");
 
 const app =express();
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth",authRoutes);
+app.use("/api/jobs",jobRoutes);
 
 app.get("/health",(req,res)=>{
     res.json({status:"ok,Jan 28"});
